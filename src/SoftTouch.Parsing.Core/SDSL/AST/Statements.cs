4f74b944b05594743a674cdfd9878faf3e999e84
Youness KAFIA <kafia.youness@gmail.com>
2024-02-13 17:55:35 +0100

    more statement nodes

--- src/SoftTouch.Parsing.Core/SDSL/AST/Statements.cs
+++ src/SoftTouch.Parsing.Core/SDSL/AST/Statements.cs
@@ -1,6 +1,5 @@
 namespace SoftTouch.Parsing.SDSL.AST;
 
-
 public abstract class Statement(TextLocation info) : ValueNode(info);
 
 
@@ -30,15 +29,35 @@ public class Assign(Expression assigned, AssignOperator op, Expression value, Te
 
 public abstract class Flow(TextLocation info) : Statement(info);
 
-public class ForEach(Identifier typename, Identifier variable, Expression collection, TextLocation info) : Flow(info)
+public abstract class Loop(TextLocation info) : Flow(info);
+
+public class ForEach(Identifier typename, Identifier variable, Expression collection, TextLocation info) : Loop(info)
 {
     public Identifier Typename { get; set; } = typename;
     public Identifier Variable { get; set; } = variable;
     public Expression Collection { get; set; } = collection;
+    public List<Statement> Body { get; set; } = [];
 }
 
-public class While(Expression condition, Expression collection, TextLocation info) : Flow(info)
+
+public abstract class ForInitializer(TextLocation info) : Node(info);
+public abstract class InitializerDeclare(Identifier typename, Identifier name, Expression value, TextLocation info) : Node(info)
+{
+    public Identifier TypeName { get; set; } = typename;
+    public Identifier VariableName { get; set; } = name;
+    public Expression Value { get; set; } = value;
+}
+
+public class For(ForInitializer initializer, Expression cond, Statement update, TextLocation info) : Loop(info)
+{
+    public ForInitializer Initializer { get; set; } = initializer;
+    public Expression Condition { get; set; } = cond;
+    public Statement Update { get; set; } = update;
+    public List<Statement> Body { get; set; } = [];
+}
+
+public class While(Expression condition, TextLocation info) : Loop(info)
 {
     public Expression Condition { get; set; } = condition;
-    public Expression Collection { get; set; } = collection;
+    public List<Statement> Body { get; set; } = [];
 }
